0ccf3786ac06ce62b6af8d5414b80dd62cd15c80
Aarti Chella <aartichella@gmail.com>
2020-11-20 09:38:44 -0500

    Added File Upload Functionality

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -1,5 +1,6 @@
 import "./Hero.scss";
 import MainContent from '../MainContent/MainContent';
+import UploadFile from '../UploadFile/UploadFile';
 
 function Hero() {
   return (
@@ -8,6 +9,7 @@ function Hero() {
         <h1 className="hero__heading">Uncle Joe's Farm Feast</h1>
       </div>
       <MainContent/>
+      <UploadFile/>
     </>
   );
 }
--- server/index.js
+++ server/index.js
@@ -4,7 +4,10 @@ const app = express();
 const PORT = 8080;
 const fs = require('fs');
 const cors = require('cors');
+const multer = require('multer');
+const fileUpload = multer({ dest: 'uploads/' });
 
+app.use(express.static("files"));
 app.use(cors());
 app.use(express.json());
 'use strict';
@@ -13,10 +16,17 @@ const excelToJson = require('convert-excel-to-json');
 const result = excelToJson({
 	sourceFile: './sampleData.xlsx'
 });
-  app.get("/", (req,res)=>{
+
+app.get("/", (req,res)=>{
     console.log(result);
     //res.send("You are here!!!");
     res.status(200).json(result);
+})
+
+app.post("/upload", fileUpload.single('file'),(req,res)=>{
+    console.log(req.file);
+    //write fn to convert file to json
+    res.send("You are  in POST!!!");
 })
 
 app.listen(PORT, () => {
